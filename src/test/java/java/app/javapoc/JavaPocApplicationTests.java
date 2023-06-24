f2ad82a120fa8c8105dc37ac23d125ea18938f8f
Sam Wells <samwells95@gmail.com>
2023-06-24 10:27:14 -0700

    Add initial project

---
+++ src/test/java/java/app/javapoc/JavaPocApplicationTests.java
@@ -0,0 +1,13 @@
+package java.app.javapoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
